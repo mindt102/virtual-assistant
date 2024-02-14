36cace23fb066a0619e1755c9df59b2ee70a4e2f
mindt102 <52208218+mindt102@users.noreply.github.com>
2024-02-14 22:28:14 +0700

    [add] pause

--- views/youtube/VideoView.py
+++ views/youtube/VideoView.py
@@ -1,9 +1,34 @@
 import discord
-from discord.ui import view, button
+from discord.ui import view, button, Modal, TextInput
 from config import FEEDBACK_TIMEOUT
 from utils.logging_utils import setup_logger, unexpected_error_handler
 
 from queries.youtube_queries import update_watched_at
+
+class PauseTimeModal(Modal):
+    timestamp = TextInput(
+        label="Timestamp", placeholder="Colon seperated timestamp (e.g.: 1:02:03)", style=discord.TextStyle.short)
+
+    def __init__(self, view, **kwargs):
+        super().__init__(**kwargs)
+        self.view: VideoView = view
+        # logger.info(f"View: {id(self.view)}")
+        # logger.info(f"Self view: {id(self.view)}")
+
+    async def on_submit(self, interaction: discord.Interaction):
+        # Convert timestamp to seconds
+        timestamp = self.timestamp.value.split(":")
+        if len(timestamp) == 3:
+            seconds = int(timestamp[0])*3600 + int(timestamp[1])*60 + int(timestamp[2])
+        elif len(timestamp) == 2:
+            seconds = int(timestamp[0])*60 + int(timestamp[1])
+        elif len(timestamp) == 1:
+            seconds = int(timestamp[0])
+        else:
+            await interaction.response.send_message("Invalid timestamp", ephemeral=True, delete_after=FEEDBACK_TIMEOUT)
+            return
+
+        await interaction.response.edit_message(content=f"https://www.youtube.com/watch?v={self.view.video_id}&t={seconds}", view=self.view)
 
 
 class VideoView(view.View):
@@ -27,6 +52,14 @@ class VideoView(view.View):
     async def skip(self, interaction: discord.Interaction, button: discord.ui.Button):
         try:
             await interaction.response.edit_message(content="⏩", view=None, delete_after=FEEDBACK_TIMEOUT)
+        except Exception as e:
+            await interaction.response.send_message(f"Server Error", ephemeral=True)
+            unexpected_error_handler(self.logger, e)
+
+    @button(label="Pause", style=discord.ButtonStyle.secondary)
+    async def pause(self, interaction: discord.Interaction, button: discord.ui.Button):
+        try:
+            await interaction.response.send_modal(PauseTimeModal(title="Timestamp to pause", view=self))
         except Exception as e:
             await interaction.response.send_message(f"Server Error", ephemeral=True)
             unexpected_error_handler(self.logger, e)
